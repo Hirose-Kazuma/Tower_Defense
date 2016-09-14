8bfa5253079757904a5e9b53f3f8311bdf5b5429
yamayamada <nkc.ryu.yamada@gmail.com>
2016-09-14 11:42:05 +0900

    AppDelegate修正

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -1,12 +1,13 @@
 #include "AppDelegate.h"
-#include "HelloWorldScene.h"
+#include "MultiResolution.h"
+#include "MainScene.h"
 
 USING_NS_CC;
 
-static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
-static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
-static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
-static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
+//static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
+//static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
+//static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
+//static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
 
 AppDelegate::AppDelegate() {
 
@@ -35,52 +36,80 @@ static int register_all_packages()
 }
 
 bool AppDelegate::applicationDidFinishLaunching() {
-    // initialize director
-    auto director = Director::getInstance();
-    auto glview = director->getOpenGLView();
-    if(!glview) {
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glview = GLViewImpl::createWithRect("Tower_Defense", Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
-#else
-        glview = GLViewImpl::create("Tower_Defense");
-#endif
-        director->setOpenGLView(glview);
-    }
+	// initialize director
+	auto director = Director::getInstance();
+	auto glview = director->getOpenGLView();
+	if (!glview) {
+		glview = GLViewImpl::create("Tower_Defense");
+		director->setOpenGLView(glview);
+	}
 
-    // turn on display FPS
-    director->setDisplayStats(true);
+	// turn on display FPS
+	director->setDisplayStats(true);
 
-    // set FPS. the default value is 1.0/60 if you don't call this
-    director->setAnimationInterval(1.0 / 60);
+	// set FPS. the default value is 1.0/60 if you don't call this
+	director->setAnimationInterval(1.0 / 60);
 
-    // Set the design resolution
-    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::NO_BORDER);
-    Size frameSize = glview->getFrameSize();
-    // if the frame's height is larger than the height of medium size.
-    if (frameSize.height > mediumResolutionSize.height)
-    {        
-        director->setContentScaleFactor(MIN(largeResolutionSize.height/designResolutionSize.height, largeResolutionSize.width/designResolutionSize.width));
-    }
-    // if the frame's height is larger than the height of small size.
-    else if (frameSize.height > smallResolutionSize.height)
-    {        
-        director->setContentScaleFactor(MIN(mediumResolutionSize.height/designResolutionSize.height, mediumResolutionSize.width/designResolutionSize.width));
-    }
-    // if the frame's height is smaller than the height of medium size.
-    else
-    {        
-        director->setContentScaleFactor(MIN(smallResolutionSize.height/designResolutionSize.height, smallResolutionSize.width/designResolutionSize.width));
-    }
+	register_all_packages();
 
-    register_all_packages();
+	// create a scene. it's an autorelease object
+	//auto scene = HelloWorld::createScene();
+	//auto scene = GameScene::create();
+	auto scene = MainScene::createScene();
 
-    // create a scene. it's an autorelease object
-    auto scene = HelloWorld::createScene();
+	// ‰ð‘œ“x‚ÌÝ’è
+	glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::SHOW_ALL);
+	// run
+	director->runWithScene(scene);
 
-    // run
-    director->runWithScene(scene);
+	return true;
 
-    return true;
+//    // initialize director
+//    auto director = Director::getInstance();
+//    auto glview = director->getOpenGLView();
+//    if(!glview) {
+//#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
+//        glview = GLViewImpl::createWithRect("Tower_Defense", Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+//#else
+//        glview = GLViewImpl::create("Tower_Defense");
+//#endif
+//        director->setOpenGLView(glview);
+//    }
+//
+//    // turn on display FPS
+//    director->setDisplayStats(true);
+//
+//    // set FPS. the default value is 1.0/60 if you don't call this
+//    director->setAnimationInterval(1.0 / 60);
+//
+//    // Set the design resolution
+//    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::NO_BORDER);
+//    Size frameSize = glview->getFrameSize();
+//    // if the frame's height is larger than the height of medium size.
+//    if (frameSize.height > mediumResolutionSize.height)
+//    {        
+//        director->setContentScaleFactor(MIN(largeResolutionSize.height/designResolutionSize.height, largeResolutionSize.width/designResolutionSize.width));
+//    }
+//    // if the frame's height is larger than the height of small size.
+//    else if (frameSize.height > smallResolutionSize.height)
+//    {        
+//        director->setContentScaleFactor(MIN(mediumResolutionSize.height/designResolutionSize.height, mediumResolutionSize.width/designResolutionSize.width));
+//    }
+//    // if the frame's height is smaller than the height of medium size.
+//    else
+//    {        
+//        director->setContentScaleFactor(MIN(smallResolutionSize.height/designResolutionSize.height, smallResolutionSize.width/designResolutionSize.width));
+//    }
+//
+//    register_all_packages();
+//
+//    // create a scene. it's an autorelease object
+//    auto scene = HelloWorld::createScene();
+//
+//    // run
+//    director->runWithScene(scene);
+//
+//    return true;
 }
 
 // This function will be called when the app is inactive. When comes a phone call,it's be invoked too
