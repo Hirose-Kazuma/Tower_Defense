932c0528e5d30685e7f58f7cb385a6ad9c0a6cb4
Hirose-Kazuma <nkc.kazuma.hirose@gmail.com>
2016-09-29 11:30:18 +0900

    敵追加

---
+++ Classes/ZakoSprite.cpp
@@ -0,0 +1,35 @@
+#include "ZakoSprite.h"
+
+USING_NS_CC;
+
+bool ZakoSprite::init()
+{
+	if (!Sprite::init())
+	{
+		return false;
+	}
+
+	countFlg = 0;
+
+	zako = Sprite::create("zako_1.png");
+	addChild(zako);
+
+	scheduleUpdate();
+
+	return true;
+}
+
+void ZakoSprite::update(float delta)
+{
+	countFlg++;
+
+	if (countFlg == 10)
+	{
+		zako->setTexture("zako_2.png");
+	}
+	else if (countFlg == 20)
+	{
+		zako->setTexture("zako_1.png");
+		countFlg = 0;
+	}
+}
