6c6edd887e57261b87ec014512af12dc356b0bdd
Hirose-Kazuma <nkc.kazuma.hirose@gmail.com>
2016-09-29 10:18:56 +0900

    カラス関連

---
+++ Classes/CrowSprite.cpp
@@ -0,0 +1,35 @@
+#include "CrowSprite.h"
+
+USING_NS_CC;
+
+bool CrowSprite::init()
+{
+	if (!Sprite::init())
+	{
+		return false;
+	}
+
+	countFlg = 0;
+
+	Crow = Sprite::create("Crow_1.png");
+	addChild(Crow);
+
+	scheduleUpdate();
+
+	return true;
+}
+
+void CrowSprite::update(float delta)
+{
+	countFlg++;
+
+	if (countFlg == 10)
+	{
+		Crow->setTexture("Crow_2.png");
+	}
+	else if (countFlg==20)
+	{
+		Crow->setTexture("Crow_1.png");
+		countFlg = 0;
+	}
+}
